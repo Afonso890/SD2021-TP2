9a3e6605d2b9b84e5c2e0c2842a43be04274b19c
Afonso890 <afonso.simaozinho@gmail.com>
2021-05-15 12:09:26 +0100

    'update1'

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# base image
+FROM nunopreguica/sd2021tpbase
+
+# working directory inside docker image
+WORKDIR /home/sd
+
+# copy the jar created by assembly to the docker image
+COPY target/*jar-with-dependencies.jar sd2021.jar
+
+# copy keystore
+COPY server.ks server.ks
+
+# copy keystore
+COPY sheets.ks sheets.ks
+
+# copy truststore
+COPY truststore.ks truststore.ks
+
+# copy the file of properties to the docker image
+COPY trab.props trab.props
+
+# run Discovery when starting the docker image
+CMD ["java", "-cp", "/home/sd/sd2021.jar", \
+"sd2021.aula2.server.UserServer"]
---
+++ src/tp1/impl/srv/soap/SpreadsheetsSoapServer.java
@@ -0,0 +1,64 @@
+package tp1.impl.srv.soap;
+
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsServer;
+
+import jakarta.xml.ws.Endpoint;
+import tp1.impl.discovery.Discovery;
+import tp1.impl.srv.Domain;
+import tp1.impl.utils.IP;
+import tp1.impl.utils.InsecureHostnameVerifier;
+
+
+public class SpreadsheetsSoapServer {
+
+	public static final int PORT = 14567;
+	public static final String SERVICE_NAME = "sheets";
+	public static String SERVER_BASE_URI = "https://%s:%s/soap";
+	public static final String SOAP_USERS_PATH = "/soap/sheets";
+
+	private static Logger Log = Logger.getLogger(SpreadsheetsSoapServer.class.getName());
+
+	public static void main(String[] args) throws Exception {
+		Domain.set(args.length > 0 ? args[0] : "?");
+
+		Log.setLevel(Level.FINER);
+
+		String ip = IP.hostAddress();
+		String serverURI = String.format(SERVER_BASE_URI, ip, PORT);
+		String FullServiceName = String.format("%s:%s", Domain.get(), SERVICE_NAME);
+
+		//This allows client code executed by this server to ignore hostname verification
+		HttpsURLConnection.setDefaultHostnameVerifier(new InsecureHostnameVerifier());
+		
+		//Create an https configurator to define the SSL/TLS context
+		HttpsConfigurator configurator = new HttpsConfigurator(SSLContext.getDefault());
+		
+		HttpsServer server = HttpsServer.create(new InetSocketAddress(ip, PORT), 0);
+		
+		server.setHttpsConfigurator(configurator);
+		
+		server.setExecutor(Executors.newCachedThreadPool());
+		
+
+		Discovery.getInstance().announce(String.format("%s:%s",Domain.get(), SERVICE_NAME), serverURI);
+
+		Endpoint soapUsersEndpoint = Endpoint.create(new SoapUsersWebService());
+		
+		soapUsersEndpoint.publish(server.createContext(SOAP_USERS_PATH));
+		
+		server.start();
+
+		Log.info(String.format("%s Soap Server ready @ %s\n", SERVICE_NAME, serverURI));
+
+	}
+}
